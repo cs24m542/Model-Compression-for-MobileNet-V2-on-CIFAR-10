49b8f7899ffe35b5a4e2297fa6f85922c53c9caa
cs24m542 <cs24m542@smail.iitm.ac.in>
2025-12-14 23:23:44 +0530

    Update utility.py

--- utility.py
+++ utility.py
@@ -1,10 +1,28 @@
 import torch
 import os
 import math
+import random
+import numpy as np
 
 # ------------------------------
 # DEVICE
 # ------------------------------
+def set_seed(seed: int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+
+    # For reproducible CUDA algorithms (optional)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    os.environ["PYTHONHASHSEED"] = str(seed)
+
+
 def get_device():
     if torch.cuda.is_available():
         return torch.device("cuda")
